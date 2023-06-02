77ccc754daf9b79c7f032737ed44310751a3355f
Ihor Babych <archi.ihor@gmail.com>
2023-06-02 21:46:39 +0300

    changed 2/0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+
 const galleryDiv = document.querySelector(".gallery");
 const allGallery = galleryMarkup(galleryItems);
  
+
 function galleryMarkup(galleryItems) {
     return galleryItems.map((galleryItem) => {
         return `
@@ -19,24 +22,30 @@ function galleryMarkup(galleryItems) {
     })
     .join("")
 };
+
 galleryDiv.insertAdjacentHTML("beforeend", allGallery)
+
 galleryDiv.addEventListener(`click`, e => {
     e.preventDefault();
     if (e.target.nodeName !== `IMG`) {
         return;
     }
+
     instance.element().querySelector(`img`).src = e.target.dataset.source;
+
     instance.show();
 });
+
 window.addEventListener(`keydown`, e => {
     if (e.code === `Escape`) {
         instance.close();
     }
 });
+
 const instance = basicLightbox.create(`<img src="" />`, {
   onShow: instance => ('onShow', instance),
   onClose: instance => ('onClose', instance),
-});    
+});
 
 // console.log(galleryItems);
 
